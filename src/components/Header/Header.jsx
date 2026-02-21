eb7208b0687f80655bac4fcc682d4c57239128d7
Anturia Hassani <anturia.hm@gmail.com>
2026-02-21 22:38:59 +0100

    intégration du header ; logo placé

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,21 @@
 import './Header.css'
+import logo from '../../assets/logo.png'
 
 function Header() {
     return (
         <header>
             <section className="header">
-                
+                <div className="logo">
+                    <img className="logo-img" src={logo} />
+                </div>
+                <div className="titles">
+                    <h1 className="title">Bienvenue sur Pic-Share</h1>
+                    <h2 className='subtitle'>Partagez vos souvenirs</h2>
+                </div>
+                <div className="buttons">
+                    <button className='connexion'>Connexion</button>
+                    <button className='inscription'>Inscription</button>
+                </div>
             </section>
         </header>
     );

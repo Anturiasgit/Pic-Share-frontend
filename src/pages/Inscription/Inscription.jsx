62d612339aa1f7a8997ffd26c4096d06d6bede87
Anturia Hassani <anturia.hm@gmail.com>
2026-02-28 02:29:27 +0100

    inscription page finished

--- src/pages/Inscription/Inscription.jsx
+++ src/pages/Inscription/Inscription.jsx
@@ -3,7 +3,8 @@ import './Inscription.css'
 
 function Inscription () {
     const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [password1, setPassword1] = useState("");
+    const [password2, setPassword2] = useState("");
     const [pseudo, setPseudo] = useState("");
     
         const handleSignIn = async (e) => {
@@ -27,7 +28,12 @@ function Inscription () {
 
         return(
             <section className="inscription-section">
+                <div className="inscription-title">
+                    <h2 className="title">Inscription</h2>
+                </div>
+
                   <form onSubmit={handleSignIn}>
+                 <label htmlFor="email">Entrez votre email</label>       
                 <input 
                     type="email"
                     placeholder="Email"
@@ -35,13 +41,23 @@ function Inscription () {
                     onChange={(e) => setEmail(e.target.value)}
                 />
 
+                <label htmlFor="password1">Créez un mot de passe</label>
                 <input 
                     type="password"
                     placeholder="Mot de passe"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                    value={password1}
+                    onChange={(e) => setPassword1(e.target.value)}
                 />
 
+                <label htmlFor="password2">Ressaisissez votre mot de passe</label>
+                <input 
+                    type="password"
+                    placeholder="Ressaisissez le mot de passe"
+                    value={password2}
+                    onChange={(e) => setPassword2(e.target.value)}
+                />
+
+                 <label htmlFor="pseudo">Choisissez un pseudo</label>   
                  <input 
                     type="text"
                     placeholder="Choisissez un pseudo"
